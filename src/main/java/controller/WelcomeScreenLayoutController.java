d290ef8904e7e53231c9d6636fec75b082b146a6
NicolasUrban2 <nclsurban@gmail.com>
2023-03-24 17:03:26 +0100

    Ajout de l'écran titre avec la loutre flottante

---
+++ src/main/java/controller/WelcomeScreenLayoutController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import main.HelloApplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WelcomeScreenLayoutController implements Initializable {
+    @FXML
+    private ImageView imageLoutre;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loutreAnimationSetup();
+    }
+
+    private void loutreAnimationSetup() {
+        int longueurAnimation = 10;
+
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(2), imageLoutre);
+        transition.setFromY(imageLoutre.getY());
+        transition.setToY(imageLoutre.getY()+longueurAnimation);
+        transition.setCycleCount(Animation.INDEFINITE);
+        transition.setAutoReverse(true);
+
+        transition.play();
+    }
+}
