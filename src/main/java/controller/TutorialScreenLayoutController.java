a359f93ba7898e3247ec727af2aae0e94e5e57e0
HichamGsb <hichamgsb.pro@gmail.com>
2023-04-01 14:15:50 +0200

    Ajout d'une flèche dans le menu de Tutoriel de l'écran principal indiquant le fait de scroller

--- src/main/java/controller/TutorialScreenLayoutController.java
+++ src/main/java/controller/TutorialScreenLayoutController.java
@@ -1,12 +1,44 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import main.Main;
 
-public class TutorialScreenLayoutController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TutorialScreenLayoutController implements Initializable {
+
+    @FXML
+    private ImageView arrowImageView;
     private MainController mainController = MainController.getInstance();
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image image = new Image(Main.class.getResource("/images/arrow.png").toString());
+        arrowImageView.setImage(image);
+        ArrowAnimationSetup();
+    }
 
     @FXML
     private void onRetourButtonClicked() {
         mainController.changeView("welcomeScreenLayout");
     }
+
+    private void ArrowAnimationSetup() {
+        int longueurAnimation = 10;
+
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.75), arrowImageView);
+        transition.setFromY(arrowImageView.getY());
+        transition.setToY(arrowImageView.getY()+longueurAnimation);
+        transition.setCycleCount(Animation.INDEFINITE);
+        transition.setAutoReverse(true);
+
+        transition.play();
+    }
+
 }
